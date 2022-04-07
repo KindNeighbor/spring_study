378f79ceba260debbd32e8e0e7f5432b56d1dfb9
KindNeighbor <97837003+KindNeighbor@users.noreply.github.com>
2022-04-07 22:49:43 +0900

    Update 4.7

--- 스프링 기본/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 스프링 기본/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package hello.core.order;
 
 import com.sun.source.tree.UsesTree;
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -8,19 +9,21 @@ import hello.core.member.MemoryMemberRepository;
 import jdk.jfr.StackTrace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
