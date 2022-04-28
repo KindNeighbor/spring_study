0bab0fdd65339f2832db4d3b39b2c0e2e1bdb97c
KindNeighbor <97837003+KindNeighbor@users.noreply.github.com>
2022-04-29 00:14:51 +0900

    4.29

--- JPA 기본/src/main/java/hellojpa/JpaMain.java
+++ JPA 기본/src/main/java/hellojpa/JpaMain.java
@@ -17,15 +17,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
 
-            for(Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+            //영속
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("==================");
 
             tx.commit();
         } catch (Exception e) {
