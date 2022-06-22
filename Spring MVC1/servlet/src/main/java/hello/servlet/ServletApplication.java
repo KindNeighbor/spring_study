5cb953eeae677be7b7ae0eaa6bd7916192066b17
YoungJoon_Choi <zerosix1017@gmail.com>
2022-06-22 22:44:48 +0900

    6.22

--- Spring MVC1/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ Spring MVC1/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,7 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ServletComponentScan //서블릿 자동 등록
@@ -11,5 +12,4 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-
 }
